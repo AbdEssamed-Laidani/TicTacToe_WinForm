10c30fe9e18743343add2955fea7f823522e7349
AbdEssamed Laidani lap <abdolaidani0656@gmail.com>
2026-04-21 19:15:02 +0100

    refact: change main form name and add icon

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -64,9 +64,10 @@
             // pbMainBackground
             // 
             this.pbMainBackground.Image = global::TicTacToe.Properties.Resources.Background;
-            this.pbMainBackground.Location = new System.Drawing.Point(-76, -39);
+            this.pbMainBackground.Location = new System.Drawing.Point(-101, -48);
+            this.pbMainBackground.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbMainBackground.Name = "pbMainBackground";
-            this.pbMainBackground.Size = new System.Drawing.Size(935, 549);
+            this.pbMainBackground.Size = new System.Drawing.Size(1247, 676);
             this.pbMainBackground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbMainBackground.TabIndex = 0;
             this.pbMainBackground.TabStop = false;
@@ -77,9 +78,10 @@
             this.TicTacToeTitle.BackColor = System.Drawing.Color.Fuchsia;
             this.TicTacToeTitle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.TicTacToeTitle.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TicTacToeTitle.Location = new System.Drawing.Point(357, 18);
+            this.TicTacToeTitle.Location = new System.Drawing.Point(476, 22);
+            this.TicTacToeTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TicTacToeTitle.Name = "TicTacToeTitle";
-            this.TicTacToeTitle.Size = new System.Drawing.Size(304, 41);
+            this.TicTacToeTitle.Size = new System.Drawing.Size(389, 52);
             this.TicTacToeTitle.TabIndex = 1;
             this.TicTacToeTitle.Text = "Tic Tac Toe Game";
             // 
@@ -90,9 +92,10 @@
             this.lblTurnTitle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.lblTurnTitle.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTurnTitle.ForeColor = System.Drawing.Color.Goldenrod;
-            this.lblTurnTitle.Location = new System.Drawing.Point(17, 94);
+            this.lblTurnTitle.Location = new System.Drawing.Point(23, 116);
+            this.lblTurnTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTurnTitle.Name = "lblTurnTitle";
-            this.lblTurnTitle.Size = new System.Drawing.Size(90, 41);
+            this.lblTurnTitle.Size = new System.Drawing.Size(115, 52);
             this.lblTurnTitle.TabIndex = 16;
             this.lblTurnTitle.Text = "Turn";
             // 
@@ -103,9 +106,10 @@
             this.lblPlayer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblPlayer.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblPlayer.ForeColor = System.Drawing.Color.Linen;
-            this.lblPlayer.Location = new System.Drawing.Point(36, 158);
+            this.lblPlayer.Location = new System.Drawing.Point(48, 194);
+            this.lblPlayer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPlayer.Name = "lblPlayer";
-            this.lblPlayer.Size = new System.Drawing.Size(106, 30);
+            this.lblPlayer.Size = new System.Drawing.Size(138, 38);
             this.lblPlayer.TabIndex = 17;
             this.lblPlayer.Tag = "X";
             this.lblPlayer.Text = "Player 1";
@@ -117,9 +121,10 @@
             this.lblWinnerTitle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.lblWinnerTitle.Font = new System.Drawing.Font("Arial Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblWinnerTitle.ForeColor = System.Drawing.Color.Goldenrod;
-            this.lblWinnerTitle.Location = new System.Drawing.Point(17, 235);
+            this.lblWinnerTitle.Location = new System.Drawing.Point(23, 289);
+            this.lblWinnerTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblWinnerTitle.Name = "lblWinnerTitle";
-            this.lblWinnerTitle.Size = new System.Drawing.Size(128, 41);
+            this.lblWinnerTitle.Size = new System.Drawing.Size(163, 52);
             this.lblWinnerTitle.TabIndex = 18;
             this.lblWinnerTitle.Text = "Winner";
             // 
@@ -130,9 +135,10 @@
             this.lblGameWinner.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.lblGameWinner.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblGameWinner.ForeColor = System.Drawing.Color.LimeGreen;
-            this.lblGameWinner.Location = new System.Drawing.Point(36, 295);
+            this.lblGameWinner.Location = new System.Drawing.Point(48, 363);
+            this.lblGameWinner.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblGameWinner.Name = "lblGameWinner";
-            this.lblGameWinner.Size = new System.Drawing.Size(143, 30);
+            this.lblGameWinner.Size = new System.Drawing.Size(187, 38);
             this.lblGameWinner.TabIndex = 19;
             this.lblGameWinner.Tag = "X";
             this.lblGameWinner.Text = "In Progress";
@@ -142,9 +148,10 @@
             this.btnResetGame.BackColor = System.Drawing.Color.SlateBlue;
             this.btnResetGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnResetGame.ForeColor = System.Drawing.Color.Black;
-            this.btnResetGame.Location = new System.Drawing.Point(52, 363);
+            this.btnResetGame.Location = new System.Drawing.Point(69, 447);
+            this.btnResetGame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnResetGame.Name = "btnResetGame";
-            this.btnResetGame.Size = new System.Drawing.Size(181, 55);
+            this.btnResetGame.Size = new System.Drawing.Size(241, 68);
             this.btnResetGame.TabIndex = 20;
             this.btnResetGame.Text = "Reset Game";
             this.btnResetGame.UseVisualStyleBackColor = false;
@@ -162,9 +169,11 @@
             this.gbGameBoard.Controls.Add(this.pbCell7);
             this.gbGameBoard.Controls.Add(this.pbCell4);
             this.gbGameBoard.Controls.Add(this.pbCell1);
-            this.gbGameBoard.Location = new System.Drawing.Point(368, 80);
+            this.gbGameBoard.Location = new System.Drawing.Point(491, 98);
+            this.gbGameBoard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gbGameBoard.Name = "gbGameBoard";
-            this.gbGameBoard.Size = new System.Drawing.Size(402, 348);
+            this.gbGameBoard.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbGameBoard.Size = new System.Drawing.Size(536, 428);
             this.gbGameBoard.TabIndex = 21;
             this.gbGameBoard.TabStop = false;
             // 
@@ -172,9 +181,10 @@
             // 
             this.pbCell9.BackColor = System.Drawing.Color.Indigo;
             this.pbCell9.Image = ((System.Drawing.Image)(resources.GetObject("pbCell9.Image")));
-            this.pbCell9.Location = new System.Drawing.Point(282, 242);
+            this.pbCell9.Location = new System.Drawing.Point(376, 298);
+            this.pbCell9.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell9.Name = "pbCell9";
-            this.pbCell9.Size = new System.Drawing.Size(91, 71);
+            this.pbCell9.Size = new System.Drawing.Size(121, 87);
             this.pbCell9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell9.TabIndex = 15;
             this.pbCell9.TabStop = false;
@@ -185,9 +195,10 @@
             // 
             this.pbCell6.BackColor = System.Drawing.Color.Indigo;
             this.pbCell6.Image = ((System.Drawing.Image)(resources.GetObject("pbCell6.Image")));
-            this.pbCell6.Location = new System.Drawing.Point(282, 140);
+            this.pbCell6.Location = new System.Drawing.Point(376, 172);
+            this.pbCell6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell6.Name = "pbCell6";
-            this.pbCell6.Size = new System.Drawing.Size(91, 71);
+            this.pbCell6.Size = new System.Drawing.Size(121, 87);
             this.pbCell6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell6.TabIndex = 14;
             this.pbCell6.TabStop = false;
@@ -198,9 +209,10 @@
             // 
             this.pbCell3.BackColor = System.Drawing.Color.Indigo;
             this.pbCell3.Image = ((System.Drawing.Image)(resources.GetObject("pbCell3.Image")));
-            this.pbCell3.Location = new System.Drawing.Point(282, 34);
+            this.pbCell3.Location = new System.Drawing.Point(376, 42);
+            this.pbCell3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell3.Name = "pbCell3";
-            this.pbCell3.Size = new System.Drawing.Size(91, 71);
+            this.pbCell3.Size = new System.Drawing.Size(121, 87);
             this.pbCell3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell3.TabIndex = 13;
             this.pbCell3.TabStop = false;
@@ -211,9 +223,10 @@
             // 
             this.pbCell8.BackColor = System.Drawing.Color.Indigo;
             this.pbCell8.Image = ((System.Drawing.Image)(resources.GetObject("pbCell8.Image")));
-            this.pbCell8.Location = new System.Drawing.Point(162, 242);
+            this.pbCell8.Location = new System.Drawing.Point(216, 298);
+            this.pbCell8.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell8.Name = "pbCell8";
-            this.pbCell8.Size = new System.Drawing.Size(91, 71);
+            this.pbCell8.Size = new System.Drawing.Size(121, 87);
             this.pbCell8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell8.TabIndex = 12;
             this.pbCell8.TabStop = false;
@@ -224,9 +237,10 @@
             // 
             this.pbCell5.BackColor = System.Drawing.Color.Indigo;
             this.pbCell5.Image = ((System.Drawing.Image)(resources.GetObject("pbCell5.Image")));
-            this.pbCell5.Location = new System.Drawing.Point(162, 140);
+            this.pbCell5.Location = new System.Drawing.Point(216, 172);
+            this.pbCell5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell5.Name = "pbCell5";
-            this.pbCell5.Size = new System.Drawing.Size(91, 71);
+            this.pbCell5.Size = new System.Drawing.Size(121, 87);
             this.pbCell5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell5.TabIndex = 11;
             this.pbCell5.TabStop = false;
@@ -237,9 +251,10 @@
             // 
             this.pbCell2.BackColor = System.Drawing.Color.Indigo;
             this.pbCell2.Image = ((System.Drawing.Image)(resources.GetObject("pbCell2.Image")));
-            this.pbCell2.Location = new System.Drawing.Point(162, 34);
+            this.pbCell2.Location = new System.Drawing.Point(216, 42);
+            this.pbCell2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell2.Name = "pbCell2";
-            this.pbCell2.Size = new System.Drawing.Size(91, 71);
+            this.pbCell2.Size = new System.Drawing.Size(121, 87);
             this.pbCell2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell2.TabIndex = 10;
             this.pbCell2.TabStop = false;
@@ -250,9 +265,10 @@
             // 
             this.pbCell7.BackColor = System.Drawing.Color.Indigo;
             this.pbCell7.Image = ((System.Drawing.Image)(resources.GetObject("pbCell7.Image")));
-            this.pbCell7.Location = new System.Drawing.Point(41, 242);
+            this.pbCell7.Location = new System.Drawing.Point(55, 298);
+            this.pbCell7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell7.Name = "pbCell7";
-            this.pbCell7.Size = new System.Drawing.Size(91, 71);
+            this.pbCell7.Size = new System.Drawing.Size(121, 87);
             this.pbCell7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell7.TabIndex = 9;
             this.pbCell7.TabStop = false;
@@ -263,9 +279,10 @@
             // 
             this.pbCell4.BackColor = System.Drawing.Color.Indigo;
             this.pbCell4.Image = ((System.Drawing.Image)(resources.GetObject("pbCell4.Image")));
-            this.pbCell4.Location = new System.Drawing.Point(41, 140);
+            this.pbCell4.Location = new System.Drawing.Point(55, 172);
+            this.pbCell4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell4.Name = "pbCell4";
-            this.pbCell4.Size = new System.Drawing.Size(91, 71);
+            this.pbCell4.Size = new System.Drawing.Size(121, 87);
             this.pbCell4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell4.TabIndex = 8;
             this.pbCell4.TabStop = false;
@@ -276,9 +293,10 @@
             // 
             this.pbCell1.BackColor = System.Drawing.Color.Indigo;
             this.pbCell1.Image = ((System.Drawing.Image)(resources.GetObject("pbCell1.Image")));
-            this.pbCell1.Location = new System.Drawing.Point(41, 34);
+            this.pbCell1.Location = new System.Drawing.Point(55, 42);
+            this.pbCell1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pbCell1.Name = "pbCell1";
-            this.pbCell1.Size = new System.Drawing.Size(91, 71);
+            this.pbCell1.Size = new System.Drawing.Size(121, 87);
             this.pbCell1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbCell1.TabIndex = 7;
             this.pbCell1.TabStop = false;
@@ -300,21 +318,23 @@
             this.materialCard1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.materialCard1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.materialCard1.Location = new System.Drawing.Point(0, 0);
-            this.materialCard1.Margin = new System.Windows.Forms.Padding(14);
+            this.materialCard1.Margin = new System.Windows.Forms.Padding(19, 17, 19, 17);
             this.materialCard1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialCard1.Name = "materialCard1";
-            this.materialCard1.Padding = new System.Windows.Forms.Padding(14);
-            this.materialCard1.Size = new System.Drawing.Size(800, 450);
+            this.materialCard1.Padding = new System.Windows.Forms.Padding(19, 17, 19, 17);
+            this.materialCard1.Size = new System.Drawing.Size(1067, 554);
             this.materialCard1.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.materialCard1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tic Tac Toe";
             ((System.ComponentModel.ISupportInitialize)(this.pbMainBackground)).EndInit();
             this.gbGameBoard.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbCell9)).EndInit();
